873c5b5b753c7dee7574bcebc873126cb4390be7
dsokolovskiy <souldead147@gmail.com>
2024-08-07 17:26:55 +0300

    add admin panel configuration for models

--- theatre/admin.py
+++ theatre/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from theatre.models import (
+    Theatre,
+    Performance,
+    Seat,
+    Reservation,
+    Visitor
+)
+
+
+@admin.register(Theatre)
+class TheatreAdmin(admin.ModelAdmin):
+    list_display = ("name", "location")
+
+
+@admin.register(Performance)
+class PerformanceAdmin(admin.ModelAdmin):
+    list_display = ("name", "theatre", "start_time", "end_time")
+
+
+@admin.register(Seat)
+class SeatAdmin(admin.ModelAdmin):
+    list_display = ("seat_number", "performance", "is_reserved", "is_vip")
+
+
+@admin.register(Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ("seat", "visitor", "reservation_date")
+
+
+@admin.register(Visitor)
+class VisitorAdmin(admin.ModelAdmin):
+    list_display = ("name", "email")
