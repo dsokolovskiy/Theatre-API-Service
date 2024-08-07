0f4dd4814e3bd959951784e1ae2f8a9f159b176e
dsokolovskiy <souldead147@gmail.com>
2024-08-07 16:58:51 +0300

    add viewsets for Theatre, Performance, Seat, Reservation & Visitor

--- theatre/views.py
+++ theatre/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from theatre.models import (
+    Theatre,
+    Performance,
+    Seat,
+    Reservation,
+    Visitor
+)
+
+from theatre.serializers import (
+    TheatreSerializer,
+    PerformanceSerializer,
+    SeatSerializer,
+    ReservationSerializer,
+    VisitorSerializer
+)
+
+
+class TheatreViewSet(viewsets.ModelViewSet):
+    queryset = Theatre.objects.all()
+    serializer_class = TheatreSerializer
+
+
+class PerformanceViewSet(viewsets.ModelViewSet):
+    queryset = Performance.objects.all()
+    serializer_class = PerformanceSerializer
+
+
+class SeatViewSet(viewsets.ModelViewSet):
+    queryset = Seat.objects.all()
+    serializer_class = SeatSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.all()
+    serializer_class = ReservationSerializer
+
+
+class VisitorViewSet(viewsets.ModelViewSet):
+    queryset = Visitor.objects.all()
+    serializer_class = VisitorSerializer
