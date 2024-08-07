cdbdd16cde31804228ff1fa2630d632536943c51
dsokolovskiy <souldead147@gmail.com>
2024-08-07 17:38:20 +0300

    add user authentication setup

--- TheatreAPIService/settings.py
+++ TheatreAPIService/settings.py
@@ -127,3 +127,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
+}
--- TheatreAPIService/urls.py
+++ TheatreAPIService/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken import views as drf_views
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("theatre.urls")),
+    path("api-token-auth/", drf_views.obtain_auth_token),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
