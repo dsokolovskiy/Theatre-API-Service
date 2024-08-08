893f60c41bb84219ffacb7bf1b0255a2bcd6de1a
dsokolovskiy <souldead147@gmail.com>
2024-08-08 21:16:41 +0300

    write tests for views

---
+++ theatre/tests/test_views.py
@@ -0,0 +1,70 @@
+from django.test import TestCase
+from django.utils import timezone
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from datetime import datetime
+
+from theatre.models import Theatre, Performance
+
+
+class TheatreAPITest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.theatre = Theatre.objects.create(
+            name="National Theatre",
+            location="Prague, Czech Republic"
+        )
+        self.url = "/api/theatres/"
+
+    def test_theatre_list(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_theatre_create(self):
+        data = {
+            "name": "Theatre du Chatelet",
+            "location": "Paris, France"
+        }
+        response = self.client.post(self.url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["name"], "Theatre du Chatelet")
+
+
+class PerformanceAPITest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.theatre = Theatre.objects.create(
+            name="Theatre du Chatelet",
+            location="Paris, France"
+        )
+        self.start_time = timezone.make_aware(datetime(2025, 1, 10, 18, 15))
+        self.end_time = timezone.make_aware(datetime(2025, 1, 10, 20, 45))
+        self.performance = Performance.objects.create(
+            name="Madama Butterfly",
+            description="An opera by Giacomo Puccini.",
+            start_time=self.start_time,
+            end_time=self.end_time,
+            theatre=self.theatre
+        )
+        self.url = "/api/performances/"
+
+    def test_performance_list(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+
+    def test_performance_create(self):
+        data = {
+            "name": "Rigoletto",
+            "description": "An opera by Giuseppe Verdi.",
+            "start_time": timezone.make_aware(datetime(
+                2025, 2, 5, 17, 0)).isoformat(),
+            "end_time": timezone.make_aware(datetime(
+                2025, 2, 5, 19, 35)).isoformat(),
+            "theatre": self.theatre.id
+        }
+        response = self.client.post(self.url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data["name"], "Rigoletto")
