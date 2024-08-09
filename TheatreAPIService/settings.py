8b54bfc1bc2c4b17566498bbb7ab5bde1317a9d1
dsokolovskiy <souldead147@gmail.com>
2024-08-09 10:25:21 +0300

    add documentation to settings.py

--- TheatreAPIService/settings.py
+++ TheatreAPIService/settings.py
@@ -140,4 +140,12 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": (
         "rest_framework.permissions.IsAuthenticated",
     ),
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Theatre API",
+    "DESCRIPTION": "API documentation for the Theatre project",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
 }
