449cc2202bc73b7d00a6e645094d9892a9f09b11
dsokolovskiy <souldead147@gmail.com>
2024-08-09 10:26:25 +0300

    add documentation options to urls

--- TheatreAPIService/urls.py
+++ TheatreAPIService/urls.py
@@ -19,10 +19,18 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework.authtoken import views as drf_views
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView
+)
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("theatre.urls")),
     path("api-token-auth/", drf_views.obtain_auth_token),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc-ui"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
